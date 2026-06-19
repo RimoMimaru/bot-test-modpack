a1f54a5c8b4432d56c7653c8f490d7e7b0e07f01
RimoMimaru <vietthuannguyen760@gmail.com>
2026-06-19 07:51:11 +0700

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.listen(PORT, () => console.log(`Web server dang chay tren cong ${PORT}`));
 const botOptions = {
   host: '51.161.208.84',           // 🌟 ĐÃ ĐỔI: IP server mới
   port: 4770,                      // 🌟 ĐÃ ĐỔI: Port server mới
-  username: 'Server_247',          // Tên nhân vật của Bot
+  username: 'Server_247_2',          // Tên nhân vật của Bot
   version: '1.21.11',               // Phiên bản Minecraft
   viewDistance: 'tiny'             // Giảm tầm nhìn để tiết kiệm RAM
 };
